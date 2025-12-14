e9710ffd87309b79e7a4bc70b994c75265df509d
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-12-14 12:24:22 +0300

    task one

---
+++ dom-manipulation/script.js
@@ -0,0 +1,33 @@
+const quotes = [
+  {
+    text: "The only way to do great work is to love what you do.",
+    category: "Inspiration",
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    category: "Life",
+  },
+];
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuoteText = quotes[randomIndex].text;
+  const randomQuoteCategory = quotes[randomIndex].category;
+  const p = document.createElement("p");
+  const displayer = document.getElementById("quoteDisplay");
+  p.innerText = `${randomQuoteText} from category ${randomQuoteCategory}`;
+  displayer.appendChild(p);
+}
+document.getElementById("newQuote").addEventListener("click", () => {
+  showRandomQuote();
+});
+function addQuote() {
+  const text = document.getElementById("newQuoteText");
+  const category = document.getElementById("newQuoteCategory");
+  quotes.push({
+    text: text.value,
+    category: category.value,
+  });
+  text.value = "";
+  category.value = "";
+  alert("quotes added");
+}
